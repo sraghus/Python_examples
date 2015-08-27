8c95238532330ebaf7f8dd993e05fa5b558bb62e
Raghu <raghu@agiliq.com>
2015-08-27 16:03:54 +0530

    some examples

--- ex-1.py
+++ ex-1.py
@@ -5,7 +5,31 @@ import sys
 
 # Gather our code in a main() function
 def main():
-    print 'Hello, World!'
+    #print 'Hello, World!'
+    #print 'Jack and Jill went up a hill'
+    #print 'Jack fell down, and broke his crown,'
+    #print 'and Jill came tumbling after.'
+    #print 'to fetch a pail of water;'
+    #print("2 + 2 is", 2 + 2)
+    #print("3 * 4 is", 3 * 4)
+    #print("100 - 1 is", 100-1)
+    #print("(33 + 2) / 5 + 11.5 is", (33 + 2) / 5 + 11.5)
+    #print()
+    #print("Firstish Grade")
+    #print("1 + 1 =", 1 + 1)
+    #print("2 + 4 =", 2 + 4)
+    #print("5 - 2 =", 5 - 2)
+    #print()
+    #print("Thirdish Grade")
+    #print("243 - 43 =", 243 - 43)
+    #print("12 * 4 =", 12 * 4)
+    #print("12 / 3 =", 12 / 3)
+    #print("13 / 3 =", 13 // 3, "R", 13 % 3)
+
+    print("My name is " + " " + "Birthday:" + "March 15th")
+
+    max(2132,112,32489.88)
+
     # Command line args are in sys.argv[1], sys.argv[2] ...
     # sys.argv[0] is the script name itself and can be ignored
 
@@ -15,23 +39,5 @@ if __name__ == '__main__':
     main()
 
 
-print 'Jack and Jill went up a hill'
-print 'Jack fell down, and broke his crown,'
-print 'and Jill came tumbling after.'
-print 'to fetch a pail of water;'
-print("2 + 2 is", 2 + 2)
-print("3 * 4 is", 3 * 4)
-print("100 - 1 is", 100-1)
-print("(33 + 2) / 5 + 11.5 is", (33 + 2) / 5 + 11.5)
-print()
-print("Firstish Grade")
-print("1 + 1 =", 1 + 1)
-print("2 + 4 =", 2 + 4)
-print("5 - 2 =", 5 - 2)
-print()
-print("Thirdish Grade")
-print("243 - 43 =", 243 - 43)
-print("12 * 4 =", 12 * 4)
-print("12 / 3 =", 12 / 3)
-print("13 / 3 =", 13 // 3, "R", 13 % 3)
+
 
